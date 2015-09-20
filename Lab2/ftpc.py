99ca3d17601684649aab24b2ccd3b1f7ee31309f
Sonny <theli@Outlook.com>
2015-09-20 19:33:08 -0400

    Sending files over ftp

--- Lab2/ftpc.py
+++ Lab2/ftpc.py
@@ -5,7 +5,18 @@ HOST = 'gamma'    # The remote host
 PORT = 5000              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world'.encode())
+
+filename = str(sys.argv[3])
+file = open(filename, 'rb')
+
+chunksize = 1024
+while True:
+	data = file.read(chunksize)
+	if data:
+		s.sendall(data)
+	else:
+		break
+
 data = s.recv(1024)
 s.close()
 print ('Received', repr(data))
--- Lab2/ftps.py
+++ Lab2/ftps.py
@@ -3,6 +3,15 @@ import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 5000              # Arbitrary non-privileged port
+
+# Creates recv directory
+directory = "recv"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+newfile = open("recv/file", "wb")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -10,6 +19,7 @@ conn, addr = s.accept()
 print ('Connected by', addr)
 while 1:
     data = conn.recv(1024)
+    newfile.write(data)
     if not data: break
     conn.sendall(data)
 conn.close()
