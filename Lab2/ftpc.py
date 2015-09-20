7e92e783d607f8684d6208c7d0fb9466dd48fdc6
Sonny <thisisshi@gmai.com>
2015-09-20 19:27:55 -0400

    fixing python3 new stuff

--- Lab2/ftpc.py
+++ Lab2/ftpc.py
@@ -5,7 +5,7 @@ HOST = 'gamma'    # The remote host
 PORT = 5000              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+s.sendall('Hello, world'.encode())
 data = s.recv(1024)
 s.close()
 print ('Received', repr(data))
