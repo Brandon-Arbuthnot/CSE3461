2231e4b45598d3e9618d29c4d95a98d6bb5387f4
Sonny <theli@Outlook.com>
2015-09-20 21:07:43 -0400

    MD5 checksum check

--- Lab2/ftps.py
+++ Lab2/ftps.py
@@ -20,8 +20,9 @@ conn, addr = s.accept()
 data = conn.recv(4)
 print("File Size", data.decode('utf-8'))
 data = conn.recv(20)
-print("File Name", data.decode('utf-8'))
-newfile = open("recv/"+data.decode('utf-8').strip(), "wb")
+newfilename = data.decode('utf-8').strip()
+print("File Name", newfilename)
+newfile = open("recv/"+newfilename, "wb")
 print ('Connected by', addr)
 while 1:
     data = conn.recv(1000)
@@ -32,4 +33,34 @@ while 1:
 
 # Close your streams!
 conn.close()
+newfile.close()
+
+newfile = open("recv/"+newfilename, "rb")
+file = open(newfilename, 'rb')
+
+print("Your file has been copied in recv/"+filename)
+
+oldhash = hashlib.md5()
+newhash = hashlib.md5()
+
+print("Hashing MD5 checksum for both files...")
+
+# Loop to hash the MD5 checksum for both files
+
+chunksize = 1000
+while True:
+	data = file.read(chunksize)
+	newdata = newfile.read(chunksize)
+	if data:
+		oldhash.update(data)
+		newhash.update(newdata)
+
+	else:
+		break
+
+print("The two files are bitwise identical: " + str(oldhash.hexdigest() == newhash.hexdigest()))
+
+# Always close your streams
+
+file.close()
 newfile.close()
